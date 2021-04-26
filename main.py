d3a72d198eb7446f5335c69f53568f6253947a64
ashish7042 <ashishflyingstar@gmail.com>
2021-04-25 23:50:01 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+class weather(pin):
+
+
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+
+    us_pin = input()
+    weather = weather(us_pin)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
